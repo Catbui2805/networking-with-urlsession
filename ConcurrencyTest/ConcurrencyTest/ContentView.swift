3a1953c1a87ca55cba46564ef28aa65725632b11
Nguyen Tran <NguyenTC@F99.com.vn>
2020-08-16 01:18:27 +0700

    Networking with URLSession | Concurrency and DataTasks | Challenge

--- ConcurrencyTest/ConcurrencyTest/ContentView.swift
+++ ConcurrencyTest/ConcurrencyTest/ContentView.swift
@@ -10,24 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let operaton = CalculatePrimeOperation()
+    @State private var calculationDisabled = false
+    @State private var messageCalculation: String = "Calculate Primes"
     var body: some View {
         VStack {
             Spacer()
             DatePicker(selection: .constant(Date()), label: { Text("Date")})
             Spacer()
-            Button(action: calculatePrimes, label: { Text("Calculate Primes")})
+            Button(action: calculatePrimes, label: { Text(messageCalculation)})
+                .disabled(calculationDisabled)
             Spacer()
         }
     }
     
     func calculatePrimes() {
+        enablePrimeButton(enable: false)
         DispatchQueue.global(qos: .userInitiated).async {
             for number in 0...1_000_000 {
                 let isPrimeNumber = self.isPrime(number: number)
                 print("\(number) is prime: \(isPrimeNumber)")
             }
+            self.enablePrimeButton(enable: true)
         }
     }
+    
+    func enablePrimeButton(enable: Bool) {
+        if enable {
+            calculationDisabled = false
+            messageCalculation = "Calculate Primes"
+        } else {
+            calculationDisabled = true
+            messageCalculation = "Calculating ..."
+            
+        }
+    }
+    
     
     func isPrime(number: Int) -> Bool {
         if number <= 1 {
