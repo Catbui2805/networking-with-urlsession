e02371490e2579d5274c29a9814cb56fd5910d32
Nguyen Tran <NguyenTC@F99.com.vn>
2020-08-15 23:20:48 +0700

    Networking with URLSession | Concurrency and DataTasks | Understand concurrency

---
+++ ConcurrencyTest/ConcurrencyTest/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  ConcurrencyTest
+//
+//  Created by Nguyen Tran on 8/15/20.
+//  Copyright © 2020 Nguyen Tran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            DatePicker(selection: .constant(Date()), label: { Text("Date")})
+            Spacer()
+            Button(action: calculatePrimes, label: { Text("Calculate Primes")})
+            Spacer()
+        }
+    }
+    
+    func calculatePrimes() {
+        for number in 0...1_000_000 {
+            let isPrimeNumber = isPrime(number: number)
+            print("\(number) is prime: \(isPrimeNumber)")
+        }
+    }
+    
+    func isPrime(number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        if number <= 3 {
+            return true
+        }
+        var i = 2
+        while i * i < number {
+            if number % i == 0 {
+                return false
+            }
+            i = i + 2
+        }
+        return true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
